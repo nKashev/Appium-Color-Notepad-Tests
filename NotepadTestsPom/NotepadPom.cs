02e82cb7ebccc8f19a5f86661713051e51928490
Nikolay Kashev <67652045+nKashev@users.noreply.github.com>
2024-09-30 15:30:54 +0300

    Update NotepadPom.cs

--- NotepadTestsPom/NotepadPom.cs
+++ NotepadTestsPom/NotepadPom.cs
@@ -1,41 +1,50 @@
-﻿using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers; // For ExpectedConditions
 
 namespace NotepadTestsPom
 {
     public class NotepadPage
     {
-        private readonly AndroidDriver _driver;
+        private readonly AndroidDriver<AppiumElement> _driver; // Use AppiumElement in Appium 5.x
+        private readonly WebDriverWait _wait;
 
-        public NotepadPage(AndroidDriver driver)
+        public NotepadPage(AndroidDriver<AppiumElement> driver)
         {
             _driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10)); // Wait for up to 10 seconds for elements
         }
 
         // Define elements
-        public IWebElement SkipTutorialButton => _driver.FindElement(MobileBy.Id
-            ("com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip"));
-        public IWebElement AddNoteButton => _driver.FindElement(MobileBy.Id
-            ("com.socialnmobile.dictapps.notepad.color.note:id/main_btn1"));
-        public IWebElement CreateTextNoteOption => _driver.FindElement
-            (MobileBy.AndroidUIAutomator("new UiSelector().text(\"Text\")"));
-        public IWebElement WriteNoteField => _driver.FindElement
-            (MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/edit_note"));
-        public IWebElement BackButton => _driver.FindElement
-            (MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/back_btn"));
-        public IWebElement NoteTitle(string title) => _driver.FindElement
-            (By.XPath($"//android.widget.TextView[@resource-id='com.socialnmobile.dictapps.notepad.color.note:id/title' and @text='{title}']"));
-        public IWebElement MenuButton => _driver.FindElement(MobileBy.Id
-            ("com.socialnmobile.dictapps.notepad.color.note:id/menu_btn"));
-        public IWebElement DeleteOption => _driver.FindElement
-            (MobileBy.AndroidUIAutomator("new UiSelector().text(\"Delete\")"));
-        public IWebElement ConfirmDeleteButton => _driver.FindElement(MobileBy.Id("android:id/button1"));
+        public IWebElement SkipTutorialButton => _wait.Until(ExpectedConditions.ElementToBeClickable(
+            MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip")));
+        
+        public IWebElement AddNoteButton => _wait.Until(ExpectedConditions.ElementToBeClickable(
+            MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/main_btn1")));
+        
+        public IWebElement CreateTextNoteOption => _wait.Until(ExpectedConditions.ElementToBeClickable(
+            MobileBy.AndroidUIAutomator("new UiSelector().text(\"Text\")")));
+        
+        public IWebElement WriteNoteField => _wait.Until(ExpectedConditions.ElementIsVisible(
+            MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/edit_note")));
+        
+        public IWebElement BackButton => _wait.Until(ExpectedConditions.ElementToBeClickable(
+            MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/back_btn")));
+        
+        public IWebElement NoteTitle(string title) => _wait.Until(ExpectedConditions.ElementIsVisible(
+            By.XPath($"//android.widget.TextView[@resource-id='com.socialnmobile.dictapps.notepad.color.note:id/title' and @text='{title}']")));
+        
+        public IWebElement MenuButton => _wait.Until(ExpectedConditions.ElementToBeClickable(
+            MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/menu_btn")));
+        
+        public IWebElement DeleteOption => _wait.Until(ExpectedConditions.ElementToBeClickable(
+            MobileBy.AndroidUIAutomator("new UiSelector().text(\"Delete\")")));
+        
+        public IWebElement ConfirmDeleteButton => _wait.Until(ExpectedConditions.ElementToBeClickable(
+            MobileBy.Id("android:id/button1")));
 
         // Define actions
         public void SkipTutorial()
@@ -59,4 +68,3 @@ namespace NotepadTestsPom
         public void ConfirmDelete() => ConfirmDeleteButton.Click();
     }
 }
-
